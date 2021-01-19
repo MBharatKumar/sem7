bd5b79e32133bd14396061afdb28b9853213e8f2
MBharatKumar <45728401+MBharatKumar@users.noreply.github.com>
2021-01-19 06:47:27 +0530

    Update 8.py

--- dm_lab/8.py
+++ dm_lab/8.py
@@ -1,20 +1,56 @@
-#Grouping people based on gender, age, category
+from sklearn.cluster import DBSCAN, AgglomerativeClustering
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import LabelEncoder, StandardScaler, normalize
+import pandas as pd
+import matplotlib.pyplot as plt
+import scipy.cluster.hierarchy as shc
+import numpy as np
 
-#Hierarchical Clustering (Agglomerative clustering)
-
-from sklearn.cluster import AgglomerativeClustering
 df = pd.read_csv('CustomerData.csv')
-dataframe = df[['gender', 'age', 'category']]
+df.head()
 
-agg = AgglomerativeClustering(n_clusters = 5, affinity = 'euclidean')
-agg.fit_predict(dataframe)
+df.loc[df['Age'] > 100, 'Age'] = np.nan
 
-dataframe['cluster'] = cluster.labels_
+df['Age'] = df['Age'].fillna(int(df['Age'].mean()))
+df['Gender'] = df['Gender'].fillna(df['Gender'].mode()[0])
+df['Country'] = df['Country'].fillna(df['Country'].mode()[0])
+df['Employed'] = df['Employed'].fillna(df['Employed'].mode()[0])
+df['Income'] = df['Income'].fillna(df['Income'].mean())
+df['ItemsPurchased(monthly)'] = df['ItemsPurchased(monthly)'].fillna(int(df['ItemsPurchased(monthly)'].mean()))
+df['ProductType'] = df['ProductType'].fillna(df['ProductType'].mode()[0])
+df['PaymentType'] = df['PaymentType'].fillna(df['PaymentType'].mode()[0])
+df['Mode'] = df['Mode'].fillna(df['Mode'].mode()[0])
 
-dataframe.head()
+df[['Gender', 'Country', 'Employed', 'ProductType', 'PaymentType', 'Mode']] = df[['Gender', 'Country', 'Employed', 'ProductType', 'PaymentType', 'Mode']].apply(LabelEncoder().fit_transform)
+df.head()
 
-sns.scatterplot(data = dataframe, x = dataframe.cluster, y = dataframe.age)
+X = df[['Age','Gender','Income','ItemsPurchased(monthly)']]
+y = df['Mode']
 
-sns.scatterplot(data = dataframe, x = dataframe.cluster, y = dataframe.gender.replace({0 : "Male", 1 : "Female"}))
+scaler = StandardScaler() 
+X_scaled = scaler.fit_transform(X) 
+X_normalized = normalize(X_scaled)
 
-sns.scatterplot(data = dataframe, x = dataframe.cluster, y = dataframe.category)
+X_normalized = pd.DataFrame(X_normalized)
+
+pca = PCA(n_components = 2) 
+X_principal = pca.fit_transform(X_normalized) 
+X_principal = pd.DataFrame(X_principal) 
+X_principal.columns = ['P1', 'P2']
+
+#Hierarchical Clustering
+plt.figure(figsize =(8, 8)) 
+plt.title('Visualising the data') 
+Dendrogram = shc.dendrogram((shc.linkage(X_principal, method ='ward')))
+
+#AgglomerativeClustering
+agg = AgglomerativeClustering(n_clusters = 2, affinity = 'euclidean')
+plt.figure() 
+plt.scatter(X_principal['P1'], X_principal['P2'], c = agg.fit_predict(X_principal)) 
+plt.show()
+
+#Density Based Clustering
+dbscan = DBSCAN(eps=0.1, min_samples=3)
+plt.scatter(X_principal['P1'], X_principal['P2'], c = dbscan.fit_predict(X_principal))
+plt.figure()
+plt.show()
